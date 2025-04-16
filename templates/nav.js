f48b52fb62d0121afe9c592f159d8f493389b345
Nathan Lambert <nathanl@allenai.org>
2025-04-16 11:39:26 -0700

    Update nav.js

--- templates/nav.js
+++ templates/nav.js
@@ -67,7 +67,7 @@ class NavigationDropdown extends HTMLElement {
       </div>
 
       <div class="section">
-        <h3>Open Questions (WIP)</h3>
+        <h3>Open Questions</h3>
         <ol start="17">
           <li><a href="https://rlhfbook.com/c/17-over-optimization.html">Over-optimization</a></li>
           <li><a href="https://rlhfbook.com/c/18-style.html">Style & Information</a></li>
